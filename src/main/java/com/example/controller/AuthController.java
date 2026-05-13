dca704c8a804f31a27b8d13bf5d4a19a1d72716b
Avinash Rathod <avinash.rathod2001@gmail.com>
2026-05-14 00:59:20 +0530

    spring jwt security

---
+++ src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import com.example.jwt.util.JwtUtil;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final JwtUtil jwtUtil = new JwtUtil();
+
+    @PostMapping("/login")
+    public Map<String, String> login(
+            @RequestParam String username,
+            @RequestParam String password
+    ) {
+
+        // Dummy validation
+        if(username.equals("admin")
+                && password.equals("admin123")) {
+
+            String token = jwtUtil.generateToken(username);
+
+            return Map.of("token", token);
+        }
+
+        throw new RuntimeException("Invalid Credentials");
+    }
+}
---
+++ src/main/java/com/example/jwt/util/JwtUtil.java
@@ -0,0 +1,63 @@
+package com.example.jwt.util;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET ="mysecretkeymysecretkeymysecretkey12345";
+
+    private static final Key KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    // Generate Token
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000 * 60 * 10)
+                )
+                .signWith(KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Extract Username
+    public String extractUsername(String token) {
+
+        return getClaims(token).getSubject();
+    }
+
+    // Validate Token
+    public boolean validateToken(String token, String username) {
+
+        String extractedUsername = extractUsername(token);
+
+        return extractedUsername.equals(username)
+                && !isTokenExpired(token);
+    }
+
+    // Check Expiry
+    private boolean isTokenExpired(String token) {
+
+        return getClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    // Get Claims
+    private Claims getClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
